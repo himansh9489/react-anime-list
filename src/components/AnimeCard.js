e4345ae80a79da113da7e94b8a8036c50f28e099
himansh9489 <30984612+himansh9489@users.noreply.github.com>
2021-08-20 10:30:36 +0530

    Update AnimeCard.js

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -5,7 +5,7 @@ function AnimeCard({ anime }) {
     <article className="anime-card">
       <a href={anime.url} target="_blank" rel="noreferrer">
         <figure>
-          <img src={anime.image_url} alt="Anime Image" />
+          <img src={anime.image_url} alt="Anime_not_found" />
         </figure>
         <h3>{anime.title}</h3>
       </a>
